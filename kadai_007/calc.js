71911d05b0f59e556576d83b521b788c314287c4
shii112620 <132441863+shii112620@users.noreply.github.com>
2023-07-17 13:54:18 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,15 +1,15 @@
-let num = 15;
+let num = 5;
 
 console.log(num);
 
-if ((num % 3)== 0) {
+if ((num % 3 ==0) && (num % 5 ==0)) {
+    console.log('3と5の倍数です');
+}
+
+else if ((num % 3)== 0) {
     console.log('3の倍数です');
 }
 
-if ((num % 5)== 0) {
+else if ((num % 5)== 0) {
     console.log('5の倍数です');
-}
-
-if ((num % 3 ==0) && (num % 5 ==0)) {
-    console.log('3と5の倍数です');
 }
